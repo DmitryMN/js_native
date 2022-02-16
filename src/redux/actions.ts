b70cc25dede6664bd4e48950b22db332f3ec1251
MalenkovDN <m18dm.dm@gmail.com>
2022-02-16 23:10:04 +0300

    refactoring mapDispatchToProps, add useDispatch

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -30,10 +30,12 @@ export const ChangeActionAC = (isBuying: boolean): ChangeAction => {
 };
 
 export type ChangeCurrentCurrencyType = {
+    type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY;
+    payload: { currentCurrency: string };
 };
 
-// @ts-ignore
-export const СhangeCurrentCurrencyAC = (currentCurrency: string): ChangeCurrentCurrencyType => {
+export const ChangeCurrentCurrencyAC = (currentCurrency: string): ChangeCurrentCurrencyType => {
+    return { type: ACTIONS_TYPE.CHANGE_CURRENT_CURRENCY, payload: {currentCurrency}, }
 };
 
 export type CurrencyReducersTypes = ChangeCurrencyFieldType | ChangeAction | ChangeCurrentCurrencyType;
